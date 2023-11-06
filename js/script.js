0906d2eb199f97be26aafe8d74dc904235f89a95
andrearicciotti <ricciotti.andrea1993@gmail.com>
2023-11-06 15:33:18 +0100

    add: image elements created and added to HTML document

--- js/script.js
+++ js/script.js
@@ -2,39 +2,39 @@
 // Creo array di oggetti
 const team = [
     {
-        name : "Wayne Barnett",
-        role : "Founder & CEO",
-        photo : "wayne-barnett-founder-ceo.jpg"
+        name: "Wayne Barnett",
+        role: "Founder & CEO",
+        photo: "img/wayne-barnett-founder-ceo.jpg"
     },
 
     {
-        name : "Angela Caroll",
-        role : "Chief Editor",
-        photo : "angela-caroll-chief-editor.jpg"
+        name: "Angela Caroll",
+        role: "Chief Editor",
+        photo: "img/angela-caroll-chief-editor.jpg"
     },
 
     {
-        name : "Walter Gordon",
-        role : "Office Manager",
-        photo : "walter-gordon-office-manager.jpg"
+        name: "Walter Gordon",
+        role: "Office Manager",
+        photo: "img/walter-gordon-office-manager.jpg"
     },
 
     {
-        name : "Angela Lopez",
-        role : "Social Media Manager",
-        photo : "angela-lopez-social-media-manager.jpg"
+        name: "Angela Lopez",
+        role: "Social Media Manager",
+        photo: "img/angela-lopez-social-media-manager.jpg"
     },
 
     {
-        name : "Scott Estrada",
-        role : "Developer",
-        photo : "scott-estrada-developer.jpg"
+        name: "Scott Estrada",
+        role: "Developer",
+        photo: "img/scott-estrada-developer.jpg"
     },
 
     {
-        name : "Barbara Ramos",
-        role : "Graphic Designer",
-        photo : "barbara-ramos-graphic-designer.jpg"
+        name: "Barbara Ramos",
+        role: "Graphic Designer",
+        photo: "img/barbara-ramos-graphic-designer.jpg"
     }
 ]
 
@@ -52,19 +52,56 @@ const team = [
 
 // Stampo le informazioni su ogni singolo oggetto nel DOM sotto forma di stringa
 
-const rowElem = document.querySelector('.team-section');
-let string ="";
+const rowElem = document.querySelector('.row');
+let string = "";
 
 for (let i = 0; i < team.length; i++) {
+
     const curObject = team[i];
 
     for (let key in curObject) {
-        console.log(key, curObject[key]);
+        console.log(key, "-", curObject[key]);
 
         string += `${curObject[key]}`
     }
-    console.log(string);
+    console.log("----------");
+    // console.log(string);
 }
 
 rowElem.innerHTML = string;
-console.log(rowElem);
+// console.log(rowElem);
+
+
+// Creo elementi immagine
+const teamPhotos = [];
+
+for (let i = 0; i < team.length; i++) {
+
+    const curPhoto = team[i].photo;
+    console.log(curPhoto);
+
+    let ImageElem = document.createElement('img');
+    ImageElem.src = curPhoto;
+
+    console.log(ImageElem);
+    teamPhotos.push(ImageElem);
+
+}
+
+console.log(teamPhotos);
+
+
+// Stampo elementi immagine
+for (let i = 0; i < teamPhotos.length; i++) {
+
+    const curImage = teamPhotos[i];
+
+    const card = document.createElement('div');
+    card.classList.add('col-4');
+
+    console.log(curImage);
+    card.append(curImage);
+    rowElem.append(card);
+}
+
+
